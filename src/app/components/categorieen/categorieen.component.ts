7281ef6682f0311482bfbd1cd9b1b7710d909b1b
in637vee <jorrit.veenstra@duo.nl>
2019-12-18 16:23:25 +0100

    ngFor in categorieen

--- src/app/components/categorieen/categorieen.component.ts
+++ src/app/components/categorieen/categorieen.component.ts
@@ -7,9 +7,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategorieenComponent implements OnInit {
 
-  constructor() { }
+  niveaus: any;
+  categorieen: any;
+  selectedAllniveau: any;
+  selectedAllcat: any;
+  constructor() {
+    this.niveaus = [
+      { niveau: 'Instap', selected: false },
+      { niveau: 'Jeugd', selected: false },
+      { niveau: 'Junioren', selected: false },
+      { niveau: 'Senioren', selected: false },
+    ];
+    this.categorieen = [
+      { categorie: 'Damespaar', selected: false },
+      { categorie: 'Herenpaar', selected: false },
+      { categorie: 'Mixpaar', selected: false },
+      { categorie: 'Damesgroep', selected: false },
+      { categorie: 'Herengroep', selected: false },
+    ];
+  }
+  selectAllniveau() {
+    this.selectedAllniveau = !this.selectedAllniveau;
 
+    for (let i = 0; i < this.niveaus.length; i++) {
+      this.niveaus[i].selected = this.selectedAllniveau;
+    }
+  }
+  checkIfAllSelectedniveau() {
+    let totalSelected =  0;
+    for (let i = 0; i < this.niveaus.length; i++) {
+      if (this.niveaus[i].selected) { totalSelected++; }
+    }
+    this.selectedAllniveau = totalSelected === this.niveaus.length;
+    return true;
+  }
+  selectAllcat() {
+    this.selectedAllcat = !this.selectedAllcat;
+
+    for (let i = 0; i < this.categorieen.length; i++) {
+      this.categorieen[i].selected = this.selectedAllcat;
+    }
+  }
+  checkIfAllSelectedcat() {
+    let totalSelected =  0;
+    for (let i = 0; i < this.categorieen.length; i++) {
+      if (this.categorieen[i].selected) { totalSelected++; }
+    }
+    this.selectedAllcat = totalSelected === this.categorieen.length;
+    return true;
+  }
   ngOnInit() {
   }
-
 }
