d50b195cdedb531aaaeecf18fc640ac9ebaef189
in637vee <jorrit.veenstra@duo.nl>
2019-12-27 14:23:41 +0100

    E-lijn lijst is klaar

--- src/app/components/categorieen/categorieen.component.ts
+++ src/app/components/categorieen/categorieen.component.ts
@@ -5,57 +5,132 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './categorieen.component.html',
   styleUrls: ['./categorieen.component.css']
 })
+
 export class CategorieenComponent implements OnInit {
 
-  niveaus: any;
-  categorieen: any;
-  selectedAllniveau: any;
-  selectedAllcat: any;
-  constructor() {
-    this.niveaus = [
-      { niveau: 'Instap', selected: false },
-      { niveau: 'Jeugd', selected: false },
-      { niveau: 'Junioren', selected: false },
-      { niveau: 'Senioren', selected: false },
-    ];
-    this.categorieen = [
-      { categorie: 'Damespaar', selected: false },
-      { categorie: 'Herenpaar', selected: false },
-      { categorie: 'Mixpaar', selected: false },
-      { categorie: 'Damesgroep', selected: false },
-      { categorie: 'Herengroep', selected: false },
-    ];
-  }
-  selectAllniveau() {
-    this.selectedAllniveau = !this.selectedAllniveau;
-
-    for (let i = 0; i < this.niveaus.length; i++) {
-      this.niveaus[i].selected = this.selectedAllniveau;
-    }
-  }
-  checkIfAllSelectedniveau() {
-    let totalSelected =  0;
-    for (let i = 0; i < this.niveaus.length; i++) {
-      if (this.niveaus[i].selected) { totalSelected++; }
-    }
-    this.selectedAllniveau = totalSelected === this.niveaus.length;
-    return true;
-  }
-  selectAllcat() {
-    this.selectedAllcat = !this.selectedAllcat;
-
-    for (let i = 0; i < this.categorieen.length; i++) {
-      this.categorieen[i].selected = this.selectedAllcat;
-    }
-  }
-  checkIfAllSelectedcat() {
-    let totalSelected =  0;
-    for (let i = 0; i < this.categorieen.length; i++) {
-      if (this.categorieen[i].selected) { totalSelected++; }
-    }
-    this.selectedAllcat = totalSelected === this.categorieen.length;
-    return true;
-  }
   ngOnInit() {
+    const categorieen = { elijn: {
+        instap: {
+          damespaar: false,
+          herenpaar: false,
+          mixpaar: false,
+          damesgroep: false,
+          herengroep: false,
+        },
+        jeugd: {
+          damespaar: false,
+          herenpaar: false,
+          mixpaar: false,
+          damesgroep: false,
+          herengroep: false,
+        },
+        junioren: {
+          damespaar: false,
+          herenpaar: false,
+          mixpaar: false,
+          damesgroep: false,
+          herengroep: false,
+        },
+        senioren: {
+          damespaar: false,
+          herenpaar: false,
+          mixpaar: false,
+          damesgroep: false,
+          herengroep: false,
+        }
+      },
+      dlijn: {
+        jeugd: {
+          damespaar: false,
+          herenpaar: false,
+          mixpaar: false,
+          damesgroep: false,
+          herengroep: false,
+        },
+        junioren: {
+          damespaar: false,
+          herenpaar: false,
+          mixpaar: false,
+          damesgroep: false,
+          herengroep: false,
+        },
+        senioren: {
+          damespaar: false,
+          herenpaar: false,
+          mixpaar: false,
+          damesgroep: false,
+          herengroep: false,
+        }
+      },
+      clijn: {
+        junioren: {
+          damespaar: false,
+          herenpaar: false,
+          mixpaar: false,
+          damesgroep: false,
+          herengroep: false,
+        },
+        senioren: {
+          damespaar: false,
+          herenpaar: false,
+          mixpaar: false,
+          damesgroep: false,
+          herengroep: false,
+        }
+      },
+      blijn: {
+        junioren: {
+          damespaar: false,
+          herenpaar: false,
+          mixpaar: false,
+          damesgroep: false,
+          herengroep: false,
+        },
+        senioren: {
+          damespaar: false,
+          herenpaar: false,
+          mixpaar: false,
+          damesgroep: false,
+          herengroep: false,
+        }
+      },
+      alijn: {
+        pupillen: {
+          damespaar: false,
+          herenpaar: false,
+          mixpaar: false,
+          damesgroep: false,
+          herengroep: false,
+        },
+        jeugd: {
+          damespaar: false,
+          herenpaar: false,
+          mixpaar: false,
+          damesgroep: false,
+          herengroep: false,
+        },
+        junioren1: {
+          damespaar: false,
+          herenpaar: false,
+          mixpaar: false,
+          damesgroep: false,
+          herengroep: false,
+        },
+        junioren2: {
+          damespaar: false,
+          herenpaar: false,
+          mixpaar: false,
+          damesgroep: false,
+          herengroep: false,
+        },
+        senioren: {
+          damespaar: false,
+          herenpaar: false,
+          mixpaar: false,
+          damesgroep: false,
+          herengroep: false,
+        }
+      }
+    };
   }
 }
