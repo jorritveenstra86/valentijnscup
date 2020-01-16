e0171783617b1552c8e4c4643d356279253769ef
in637vee <jorrit.veenstra@duo.nl>
2020-01-16 15:49:49 +0100

    Verbeteren JSPDF autotable

--- frontend/src/app/pages/uitvoer-pdf/uitvoer-pdf.component.ts
+++ frontend/src/app/pages/uitvoer-pdf/uitvoer-pdf.component.ts
@@ -3,33 +3,41 @@ import * as jsPDF from 'jspdf';
 import 'jspdf-autotable';
 
 @Component({
-  selector: 'app-uitvoer-pdf',
-  templateUrl: './uitvoer-pdf.component.html',
-  styleUrls: ['./uitvoer-pdf.component.css']
+    selector: 'app-uitvoer-pdf',
+    templateUrl: './uitvoer-pdf.component.html',
+    styleUrls: ['./uitvoer-pdf.component.css']
 })
 
 
 export class UitvoerPdfComponent implements OnInit {
-  @Input() selectie;
-  constructor() {
-  }
+    @Input() selectie;
 
-  ngOnInit() {
-  }
-
-  genereerPDF() {
-    const doc = new jsPDF({
-    });
-    var head = [["ID", "Country", "Rank", "Capital"]];
-    var body = [
-      [1, "Denmark", 7.526, "Copenhagen"],
-      [2, "Switzerland", 	7.509, "Bern"],
-      [3, "Iceland", 7.501, "Reykjavík"]
+    mockData = [
+        [1, 123, 'Roel Noorman', 'E-junioren', 'Damespaar', 'Combinatie', 8.35, 7.85, 0.67, 0.30, 24.300],
+        [, , 'Henk Noorman'],
+        [2, 125, 'Jorrit Veenstra', 'E-junioren', 'Damespaar', 'Combinatie', 8.25, 7.50, 0.53, 0.30, 23.300],
+        [, , 'Henk Veenstra']
     ];
-    doc.autoTable({head: head, body: body});
-    doc.autoTable({html: '#my-table'});
-    doc.save('uitslagen.pdf');
-    console.log(this.selectie);
-  }
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    genereerPDF() {
+        const doc = new jsPDF({});
+        const head = [['Plaats', 'Nummer', 'Team', 'Niveau', 'Categorie', 'Oefening', 'T', 'A', 'MW', 'Aftr', 'Score']];
+        doc.autoTable({
+            head,
+            body: this.mockData,
+            theme: 'grid',
+            headStyles: {fillColor: [252, 15, 192]},
+            bodyStyles: {fillColor: [255, 255, 255], lineColor: [252, 15, 192]}
+        });
+        doc.save('uitslagen.pdf');
+        console.log(this.selectie);
+
+    }
 
 }
