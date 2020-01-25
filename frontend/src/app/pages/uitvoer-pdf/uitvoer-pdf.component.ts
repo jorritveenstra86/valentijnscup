1ba52b55561c0853cb2511548bc31b72417400fb
Roel <rjnoorman@gmail.com>
2020-01-25 15:32:43 +0100

    Start met PDF.

--- frontend/src/app/pages/uitvoer-pdf/uitvoer-pdf.component.ts
+++ frontend/src/app/pages/uitvoer-pdf/uitvoer-pdf.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import * as jsPDF from 'jspdf';
 import 'jspdf-autotable';
 import {TeamService} from '../../shared/team.service';
+import {Oefeningen} from '../../model/entiteiten/oefening';
 
 @Component({
   selector: 'app-uitvoer-pdf',
@@ -13,32 +14,127 @@ import {TeamService} from '../../shared/team.service';
 export class UitvoerPdfComponent implements OnInit {
   @Input() selectie;
   private uitslag = [];
+  private doc = new jsPDF({});
+  filename = 'uitslagen.pdf'; // TODO invoerveld maken voor filename
+  finalY = 0;
+  eerstePagina;
 
   constructor(private teamService: TeamService) {
   }
 
   ngOnInit() {
-    this.teamService.getTeamPerCategorie('E-junior', 'Damespaar').subscribe((response: any) => {
-      this.uitslag = response.uitslag;
+  }
+
+  start() {
+    this.eerstePagina = true;
+    this.maakTabel('A-pupil', 'Damespaar');
+    this.maakTabel('A-junior 1', 'Damespaar');
+    // TODO hier komen alle combinaties van niveau en categorie
+    // ...
+    // ...
+    setTimeout(() => {
+      // We gaan opslaan. Voor de zekerheid een setTimeout...
+      this.saveDoc();
+    }, 1000);
+  }
+
+  maakTabel(niveau, categorie) {
+    this.teamService.getTeamPerCategorie(niveau, categorie).subscribe((response: any) => {
+      let entries = [];
+      response.forEach(
+        (team) => {
+          entries = entries.concat(this.createEntriesForTeam(team));
+        });
+      const titel = niveau + ' / ' + categorie;
+      console.log(titel);
+      this.genereerPDF(titel, niveau, categorie, entries);
+      this.eerstePagina = false;
     });
   }
 
-  doElijn() {
-
+  createEntriesForTeam(team) {
+    // Omdat een team meerdere oefeningen kan doen. Daarom zo ingewikkeld... :(
+    const result = [];
+    if (team.score_balans) {
+      result.push(this.createAutotableEntry(team, Oefeningen[0]));
+    }
+    if (team.score_tempo) {
+      result.push(this.createAutotableEntry(team, Oefeningen[1]));
+    }
+    if (team.score_combi) {
+      result.push(this.createAutotableEntry(team, Oefeningen[2]));
+    }
+    // we returnen een array met alle tabelEntries voor dit team
+    return result;
   }
 
-  genereerPDF() {
-    const doc = new jsPDF({});
+  createAutotableEntry(team, oefening) {
+    return [
+      null, // Plaats gaan we nog berekenen
+      null, // TODO: Wat is dit voor veld??? Nummer?
+      team.teamnummer,
+      team.niveau,
+      team.categorie,
+      oefening,
+      this.getScoreByOefening(team, oefening).technisch,
+      this.getScoreByOefening(team, oefening).artistiek,
+      this.getScoreByOefening(team, oefening).moeilijkheid,
+      this.getScoreByOefening(team, oefening).aftrekken,
+      this.getScoreByOefening(team, oefening).score
+    ];
+  }
+
+  getScoreByOefening(team, oefening) {
+    if (oefening === Oefeningen[0]) {
+      return {
+        technisch: team.technisch_balans,
+        artistiek: team.artistiek_balans,
+        moeilijkheid: team.moeilijkheid_balans,
+        aftrekken: team.aftrekken_balans,
+        score: team.score_balans
+      };
+    }
+    if (oefening === Oefeningen[1]) {
+      return {
+        technisch: team.technisch_tempo,
+        artistiek: team.artistiek_tempo,
+        moeilijkheid: team.moeilijkheid_tempo,
+        aftrekken: team.aftrekken_tempo,
+        score: team.score_tempo
+      };
+    }
+    if (oefening === Oefeningen[2]) {
+      return {
+        technisch: team.technisch_combi,
+        artistiek: team.artistiek_combi,
+        moeilijkheid: team.moeilijkheid_combi,
+        aftrekken: team.aftrekken_combi,
+        score: team.score_combi
+      };
+    }
+  }
+
+  genereerPDF(titel, niveau, categorie, entries) {
+    if (!this.eerstePagina) {
+      this.doc.addPage();
+    }
+    this.finalY = this.doc.previousAutoTable.finalY;
+    // TODO zet titel op pagina
+    // this.doc.text(titel, 14, this.finalY + 15);
     const head = [['Plaats', 'Nummer', 'Team', 'Niveau', 'Categorie', 'Oefening', 'T', 'A', 'MW', 'Aftr', 'Score']];
-    doc.autoTable({
+    this.doc.autoTable({
+      // startY: this.finalY + 20,
       head,
-      body: this.uitslag,
+      body: entries,
       theme: 'grid',
       headStyles: {fillColor: [252, 15, 192]},
       bodyStyles: {fillColor: [255, 255, 255], lineColor: [252, 15, 192]}
     });
-    doc.save('uitslagen.pdf');
+    this.finalY = this.doc.previousAutoTable.finalY;
+  }
 
+  saveDoc() {
+    this.doc.save(this.filename);
   }
 
 }
