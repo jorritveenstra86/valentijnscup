b7b0cb99143ae67f05ae49fed0074880e8cf715d
in637vee <jorrit.veenstra@duo.nl>
2020-01-28 09:23:02 +0100

    verder met slide

--- frontend/src/app/pages/presentatie/slide/slide.component.ts
+++ frontend/src/app/pages/presentatie/slide/slide.component.ts
@@ -13,21 +13,11 @@ export class SlideComponent implements OnInit {
         plaats: '',
         teamnummer: '',
         namen: '',
-        technisch_balans: '',
-        artistiek_balans: '',
-        moeilijkheidswaarde_balans: '',
-        specialeAftrekken_balans: '',
-        score_balans: '',
-        technisch_tempo: '',
-        artistiek_tempo: '',
-        moeilijkheidswaarde_tempo: '',
-        specialeAftrekken_tempo: '',
-        score_tempo: '',
-        technisch_combi: '',
-        artistiek_combi: '',
-        moeilijkheidswaarde_combi: '',
-        specialeAftrekken_combi: '',
-        score_combi: ''
+        technisch: '',
+        artistiek: '',
+        moeilijkheidswaarde: '',
+        specialeAftrekken: '',
+        score: '',
     };
 
     constructor(private teamService: TeamService) {
@@ -39,24 +29,17 @@ export class SlideComponent implements OnInit {
 
     maakScoreLijst(niveau, categorie) {
         this.teamService.getTeam(239).subscribe((response: any) => {
-            this.titel = niveau + ' ' + categorie;
             this.team.teamnummer = response.teamnummer;
             this.team.namen = response.naam1 + '\n' + response.naam2 + '\n' + (response.naam3 || '');
-            this.team.technisch_balans = response.technisch_balans;
-            this.team.artistiek_balans = response.artistiek_balans;
-            this.team.moeilijkheidswaarde_balans = response.moeilijkheid_balans;
-            this.team.specialeAftrekken_balans = response.aftrekken_balans;
-            this.team.score_balans = response.score_balans;
-            this.team.technisch_tempo = response.technisch_tempo;
-            this.team.artistiek_tempo = response.artistiek_tempo;
-            this.team.moeilijkheidswaarde_tempo = response.moeilijkheid_tempo;
-            this.team.specialeAftrekken_tempo = response.aftrekken_tempo;
-            this.team.score_tempo = response.score_tempo;
-            this.team.technisch_combi = response.technisch_combi;
-            this.team.artistiek_combi = response.artistiek_combi;
-            this.team.moeilijkheidswaarde_combi = response.moeilijkheid_combi;
-            this.team.specialeAftrekken_combi = response.aftrekken_combi;
-            this.team.score_combi = response.score_combbi;
+            Oefeningen.forEach((oefening) => {
+                const technisch = 'response.technisch_' + (oefening).toString();
+                this.titel = niveau + ' ' + categorie + ' ' + oefening;
+                this.team.technisch = JSON.parse(technisch);
+                this.team.artistiek = response.artistiek_balans;
+                this.team.moeilijkheidswaarde = response.moeilijkheid_balans;
+                this.team.specialeAftrekken = response.aftrekken_balans;
+                this.team.score = response.score_balans;
+            });
         });
     }
 
