2c1a407a63b4b3af9f1e347133d99c0660358659
jorritveenstra <jorrit_veenstra@hotmail.com>
2020-01-29 17:03:17 +0100

    succeservaringen met de slide component

--- frontend/src/app/model/entiteiten/oefening.ts
+++ frontend/src/app/model/entiteiten/oefening.ts
@@ -1,6 +1,6 @@
 export const Oefeningen = [
   'Balans',
   'Tempo',
-  'Combinatie'
+  'Combi'
 ];
 
--- frontend/src/app/pages/presentatie/slide/slide.component.ts
+++ frontend/src/app/pages/presentatie/slide/slide.component.ts
@@ -1,51 +1,43 @@
 import {Component, OnInit} from '@angular/core';
-import {Oefeningen} from '../../../model/entiteiten/oefening';
 import {TeamService} from '../../../shared/team.service';
 
 @Component({
-    selector: 'app-slide',
-    templateUrl: './slide.component.html',
-    styleUrls: ['./slide.component.css']
+  selector: 'app-slide',
+  templateUrl: './slide.component.html',
+  styleUrls: ['./slide.component.css']
 })
+
 export class SlideComponent implements OnInit {
     public titel = '';
-    public team = {
-        plaats: '',
-        teamnummer: '',
-        namen: '',
-        technisch: '',
-        artistiek: '',
-        moeilijkheidswaarde: '',
-        specialeAftrekken: '',
-        score: '',
-    };
+    public niveau = 'E-instap';
+    public categorie = 'Damespaar';
+    public oefening = 'combi';
+    public teamArray: any [][] = [];
 
-    constructor(private teamService: TeamService) {
+  constructor(private teamService: TeamService) {
     }
 
     ngOnInit() {
-        this.maakScoreLijst('B-senior', 'damesgroep');
+        this.maakScoreLijst(this.niveau, this.categorie);
     }
 
     maakScoreLijst(niveau, categorie) {
-        this.teamService.getTeam(239).subscribe((response: any) => {
-            this.team.teamnummer = response.teamnummer;
-            this.team.namen = response.naam1 + '\n' + response.naam2 + '\n' + (response.naam3 || '');
-            Oefeningen.forEach((oefening) => {
-                const technisch = 'technisch_' + (oefening).toLowerCase().toString();
-                this.titel = niveau + ' ' + categorie + ' ' + oefening;
-                this.team.technisch = response[technisch];
-                console.log(response[technisch]);
-                this.team.artistiek = response.artistiek_balans;
-                this.team.moeilijkheidswaarde = response.moeilijkheid_balans;
-                this.team.specialeAftrekken = response.aftrekken_balans;
-                this.team.score = response.score_balans;
+        this.teamService.getTeamPerCategorie(this.niveau, this.categorie).subscribe((response: any) => {
+              let count = 0;
+              response.forEach((team) => {
+                const technisch = 'technisch_' + (this.oefening).toLowerCase().toString();
+                const artistiek = 'artistiek_' + (this.oefening).toLowerCase().toString();
+                const moeilijkheidswaarde = 'moeilijkheid_' + (this.oefening).toLowerCase().toString();
+                const specialeAftrekken = 'aftrekken_' + (this.oefening).toLowerCase().toString();
+                const score = 'score_' + (this.oefening).toLowerCase().toString();
+                if (team[score] != null) {
+                  this.titel = niveau + ' ' + categorie + ' ' + this.oefening;
+                  this.teamArray[count] = [team.teamnummer, team.naam1 + '\n' + team.naam2 + '\n' + (team.naam3 || ''), team[technisch], team[artistiek], team[moeilijkheidswaarde], team[specialeAftrekken], team[score]];
+                  count++;
+                }
+              });
             });
-        });
     }
-
-    // TODO: getteampercategorie ipv getteam
-    // TODO: scores per categorie ipv alles bij elkaar
 
     sortByScore(teams) {
         return teams.sort((a, b) => {
